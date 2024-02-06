e1d1f3cc85f81f20b05da605d372c3883e2239a0
koechMR <briankipkoech328@gmail.com>
2024-02-06 16:18:08 +0300

    babel file

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Basic flask application
+"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config(object):
+    """
+    Application config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()

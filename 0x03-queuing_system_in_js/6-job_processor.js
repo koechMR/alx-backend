3fb51e40f10c883c37fcaa521d184380255d5951
koechMR <briankipkoech328@gmail.com>
2024-03-05 07:50:31 +0300

    queing system in js

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,16 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber} with message: ${message}`
+  );
+};
+
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
